d4061eb8f6f93b15c5fd19570fc2269b7973ea51
Darkk-kami <71535947+Darkk-kami@users.noreply.github.com>
2024-12-11 09:46:14 +0000

    Update instance.tf

--- modules/compute/instance.tf
+++ modules/compute/instance.tf
@@ -14,8 +14,8 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "app_instance" {
   count = var.instance_count  # Number of instances to create
 
-  instance_type          = var.instance_type
-  iam_instance_profile = var.instance_profile.id
+  instance_type          = var.environment == "dev" ? "t2.micro" : "t2.medium"
+  iam_instance_profile   = var.instance_profile.id
   ami                   = data.aws_ami.ubuntu.id
   subnet_id             = var.subnet_id[count.index % length(var.subnet_id)]
   vpc_security_group_ids = [var.security_group_id]
